558ec9991e7e9240c8cf99b61ebb9d78d0699487
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-07-11 14:56:07 -0700

    get to reading the program-file

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 )
 
 func ArrayHas(needle interface{}, haystack []interface{}) bool {
@@ -15,11 +17,22 @@ func ArrayHas(needle interface{}, haystack []interface{}) bool {
 }
 
 func main() {
-	variables := map[interface{}]interface{}{}
-	arrays := map[interface{}]interface{}{}
+	// variables := map[interface{}]interface{}{}
+	// arrays := map[interface{}]interface{}{}
+	// ifState := true
+	// willLoop := false
+	newline := regexp.MustCompile("\r?\n")
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("Looks like the command you attempted to run seems to be mal-formed. Here's what your command should look like:")
-		fmt.Printf("%s <path/to/file.bluefun>\n", os.Args[1])
+		fmt.Printf("%s <path/to/file.bluefun>\n", os.Args[0])
+		return
 	}
+	fileData, fileError := os.ReadFile(args[0])
+	if fileError != nil {
+		log.Fatalf("Failed to read %s\n", args[0])
+		return
+	}
+	program := string(fileData)
+	commands := newline.Split(program, -1)
 }
